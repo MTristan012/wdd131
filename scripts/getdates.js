d5033a0dc54228c91e7e236af7d49b9953b62d64
MTristan <53493044+MTristan012@users.noreply.github.com>
2025-01-11 19:25:04 -0600

    W01 Assignment: Course Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearElement = document.querySelector("#currentyear");
+  if (yearElement) {
+    const currentYear = new Date().getFullYear();
+    yearElement.textContent = currentYear;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lastModifiedElement = document.querySelector("#lastModified");
+  if (lastModifiedElement) {
+    const lastModifiedDate = document.lastModified;
+    lastModifiedElement.textContent = lastModifiedDate;
+  }
+});
